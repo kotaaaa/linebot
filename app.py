dedfbe3c533018617cbd5b8427d74a3c13406aa2
kotaaaa <kota.k.1132.pda@gmail.com>
2020-02-25 10:43:37 +0900

    env variable

--- app.py
+++ app.py
@@ -34,11 +34,12 @@ logger.critical('logging.critical.')
 #ログ出力関係ここまで
 
 import json
+import os
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('HmZrbJbUT+ZnBiwX5uLG8cuKg1Qo3d99+EjRPPjPCVHk1/HgDwMNR45vdJvCaIgxWmOMDnQT9z+AA9eGoKR+/vkZxGqJdZyeExDoVlreubQ73bbUfZeUpEz/AbHJqbZY/bEsXhwVMCAeiiCy15ylSgdB04t89/1O/w1cDnyilFU=')#YOUR_CHANNEL_ACCESS_TOKEN
-handler = WebhookHandler('549900161624cd875236a62d28e567a6')#YOUR_CHANNEL_SECRET
+line_bot_api = LineBotApi(os.environ.get('LINECHANNELACCESSTOKEN'))#YOUR_CHANNEL_ACCESS_TOKEN
+handler = WebhookHandler(os.environ.get('LINECHANNELSECRET'))#YOUR_CHANNEL_SECRET
 
 
 @app.route("/")
