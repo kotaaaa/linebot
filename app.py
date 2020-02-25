295fd5f8642c841ff9cd3e3bc71ee861e5a52500
kotaaaa <kota.k.1132.pda@gmail.com>
2020-02-25 11:30:06 +0900

    fwrite linetoken

--- app.py
+++ app.py
@@ -37,6 +37,9 @@ import json
 
 app = Flask(__name__)
 
+f= open("dev_log/log1", mode='a',encoding="utf-8")
+f.write(os.environ.get('LINECHANNELACCESSTOKEN'))
+
 line_bot_api = LineBotApi('HmZrbJbUT+ZnBiwX5uLG8cuKg1Qo3d99+EjRPPjPCVHk1/HgDwMNR45vdJvCaIgxWmOMDnQT9z+AA9eGoKR+/vkZxGqJdZyeExDoVlreubQ73bbUfZeUpEz/AbHJqbZY/bEsXhwVMCAeiiCy15ylSgdB04t89/1O/w1cDnyilFU=')#YOUR_CHANNEL_ACCESS_TOKEN
 handler = WebhookHandler('549900161624cd875236a62d28e567a6')#YOUR_CHANNEL_SECRET
 
@@ -59,9 +62,8 @@ def callback():
     app.logger.info("Request body: " + body)
 
     #ログをテキストファイルに保存
-    with open("dev_log/log1", mode='a',encoding="utf-8") as f:
-        body_ = json.loads(body)
-        f.write(str(body_["events"][0]["message"]["text"])+'\n')#とりあえず，これで，printする形にする．
+    body_ = json.loads(body)
+    f.write(str(body_["events"][0]["message"]["text"])+'\n')#とりあえず，これで，printする形にする．
     # handle webhook body
     try:
         handler.handle(body, signature)
