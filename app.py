d5a5bdcf61d06b81aa6ebc4b933ddd5727024697
kotaaaa <kota.k.1132.pda@gmail.com>
2020-02-25 17:39:35 +0900

    not print

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     tobuy_list = event.message.text
-    print(tobuy_list,file=sys.stderr)
+    # print(tobuy_list,file=sys.stderr)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=tobuy_list))
