6d4f57b2aad4d58f6c44fa188717245ebf25b9d2
kotaaaa <kota.k.1132.pda@gmail.com>
2020-03-03 15:57:04 +0900

    [source]

--- app.py
+++ app.py
@@ -81,8 +81,8 @@ def handle_message(event):
         f.write(str(tobuy_list)+'\n')
         # f.write(str(event["source"])+'\n')
         f.write(str(event.source)+'\n')
-        f.write(str(event.source["userId"])+'\n')
-        send_sql = u"insert into testTable values('"+"5"+"','"+event.source["userId"]+"')"
+        f.write(str(event.source.userId)+'\n')
+        send_sql = u"insert into testTable values('"+"5"+"','"+event.source.userId+"')"
         db_text_save(send_sql)
         line_bot_api.reply_message(
             event.reply_token,
