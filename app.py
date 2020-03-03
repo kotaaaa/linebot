08e8905b4eca64d3b74a6faddc936633e0604167
kotaaaa <kota.k.1132.pda@gmail.com>
2020-03-03 17:12:19 +0900

    select *

--- app.py
+++ app.py
@@ -78,19 +78,21 @@ def callback():
 def handle_message(event):
     tobuy_lists = event.message.text.split('\n')
     for tobuy_list in tobuy_lists:
-        f.write(str(tobuy_list)+'\n')
-        # f.write(str(event["source"])+'\n')
-        f.write(str(event.source)+'\n')
-        f.write(str(event.source.user_id)+'\n')
-        # send_sql = u"insert into itemTable(user_id,item,todo_flg) values('"+event.source.user_id+"','"+tobuy_list+"','1')"
+        # f.write(str(tobuy_list)+'\n')
+        # f.write(str(event.source)+'\n')
+        # f.write(str(event.source.user_id)+'\n')
         send_sql = u"INSERT INTO itemTable(user_id, item, todo_flg) VALUES('"+event.source.user_id+"','"+tobuy_list+"','1') ON DUPLICATE KEY UPDATE user_id = '"+event.source.user_id+"',item = '"+tobuy_list+"', todo_flg = 0"
 
         db_text_save(send_sql)
+
+        latest_todo_lists = u"select * from itemTable where user_id = '"+event.source.user_id+"' and todo_flg = 1;"
+        f.write(str(latest_todo_lists)+'\n')
+
         line_bot_api.reply_message(
             event.reply_token,
             [
-            TextSendMessage(text=tobuy_list),
-            TextSendMessage(text=tobuy_list)
+            TextSendMessage(text=latest_todo_lists),
+            # TextSendMessage(text=tobuy_list)
             ])
 
 def getConnection():
