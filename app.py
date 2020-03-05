02f5ad667efed5b458615f722486fde060e02ed6
kotaaaa <kota.k.1132.pda@gmail.com>
2020-03-05 18:32:59 +0900

    list

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def handle_message(event):
         select_sql = u"select item from itemTable where user_id = '"+event.source.user_id+"' and todo_flg = 1"
 
         latest_todos_lists = db_text_select(select_sql)
-        latest_todos_lists = [str(i) for i in latest_todos_lists]
+        latest_todos_lists = [str(i[0]) for i in latest_todos_lists]
         latest_todos = '\n'.join(latest_todos_lists)
         line_bot_api.reply_message(
             event.reply_token,
